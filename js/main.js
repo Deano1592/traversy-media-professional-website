58a473692882f55e31c2bdcab4d28a9efca52a4b
Dean Ridgwell <deano1592@hotmail.com>
2024-09-30 23:05:36 +0100

    Adding JS to FAQ section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// FAQ ACCORDION
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+    if (!groupHeader) return;
+    console.log(groupHeader);
+  });
+});
